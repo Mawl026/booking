835e8ca6429800187732acbb4ac1ce1224220792
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-19 17:07:22 +0200

    Update BookItemFacade.java
    Updated BookItemFacade java class.

--- src/main/java/business/services/BookItemFacade.java
+++ src/main/java/business/services/BookItemFacade.java
@@ -1,6 +1,7 @@
 package business.services;
 
 import business.entities.Booking;
+import business.exceptions.UserException;
 import business.persistence.BookingMapper;
 import business.persistence.Database;
 
@@ -9,5 +10,9 @@ public class BookItemFacade {
     public BookItemFacade(Database database){
         this.bookingMapper = new BookingMapper(database);
     }
-    public Booking bookItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id)
+    public Booking bookItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
+        Booking booking = new Booking (booking_date, days, comment, booking_status, user_id, item_id);
+        booking = bookingMapper.bookItem(booking);
+        return booking;
+    }
 }
