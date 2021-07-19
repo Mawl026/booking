d643a22900a440711da34c93fd57333d54d2dd88
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-19 18:21:32 +0200

    Update User.java
    Updated User java class,

--- src/main/java/business/entities/User.java
+++ src/main/java/business/entities/User.java
@@ -1,58 +1,66 @@
 package business.entities;
 
-public class User
-{
+public class User {
+    private int user_id; // just used to demo retrieval of autogen keys in UserMapper
+    private String email;
+    private String password; // Should be hashed and secured
+    private int phone;
+    private int credit;
+    private String role;
 
-    public User(String email, String password, String role)
-    {
+    public User(String email, String password, int phone, int credit, String role) {
         this.email = email;
         this.password = password;
+        this.phone = phone;
+        this.credit = credit;
         this.role = role;
     }
 
-    private int id; // just used to demo retrieval of autogen keys in UserMapper
-    private String email;
-    private String password; // Should be hashed and secured
-    private String role;
+    public int getUser_id() {
+        return user_id;
+    }
 
-    public String getEmail()
-    {
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email)
-    {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword()
-    {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password)
-    {
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getRole()
-    {
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getRole() {
         return role;
     }
 
-    public void setRole(String role)
-    {
+    public void setRole(String role) {
         this.role = role;
     }
-
-    public int getId()
-    {
-        return id;
-    }
-
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
 }
