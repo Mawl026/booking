33228f090f42544f65012f62ca4cbb0126298a5f
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-20 17:49:23 +0200

    Refactored bookingfacade name.
    Refactored bookingfacade class name to be more consistent with database and project structure.

--- src/main/java/business/services/BookingFacade.java
+++ src/main/java/business/services/BookingFacade.java
@@ -5,14 +5,21 @@ import business.exceptions.UserException;
 import business.persistence.BookingMapper;
 import business.persistence.Database;
 
-public class BookingAnItemFacade {
+public class BookingFacade {
+
     private BookingMapper bookingMapper;
-    public BookingAnItemFacade(Database database){
+
+    public BookingFacade(Database database) {
+
         this.bookingMapper = new BookingMapper(database);
+
     }
-    public Booking bookAnItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
+
+    public Booking bookingAnItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
+
         Booking booking = new Booking (booking_date, days, comment, booking_status, user_id, item_id);
-        booking = bookingMapper.bookItem(booking);
+        booking = bookingMapper.bookAnItem(booking);
+
         return booking;
     }
 }
