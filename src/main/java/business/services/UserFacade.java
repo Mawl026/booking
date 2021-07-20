f1004b0b50f94c42afda1fb16d03ff30fc336fc9
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-20 19:11:27 +0200

    Update UserFacade.java
    Updated code formatting.

--- src/main/java/business/services/UserFacade.java
+++ src/main/java/business/services/UserFacade.java
@@ -5,27 +5,32 @@ import business.persistence.Database;
 import business.persistence.UserMapper;
 import business.exceptions.UserException;
 
-public class UserFacade
-{
+public class UserFacade {
     UserMapper userMapper;
 
-    public UserFacade(Database database)
-    {
+
+    public UserFacade(Database database) {
         userMapper = new UserMapper(database);
+
     }
+
 
     public User login(String email, String password) throws UserException {
         return userMapper.login(email, password);
+
     }
+
 
     public User createUser(String email, String password, int phone, int credit, String role) throws UserException {
         User user = new User(email, password, phone, credit, role);
         userMapper.createUser(user);
+
         return user;
     }
 
+
     public void useCredit(int credit, int user_id) throws UserException {
         userMapper.useCredit(credit, user_id);
-    }
 
+    }
 }
