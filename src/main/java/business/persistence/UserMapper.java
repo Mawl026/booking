d7f83ef5ef4d59fb8213d4dd4b76b0d631ce979c
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-20 19:10:24 +0200

    Update UserMapper.java
    Updated code formatting.

--- src/main/java/business/persistence/UserMapper.java
+++ src/main/java/business/persistence/UserMapper.java
@@ -7,23 +7,25 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserMapper
-{
+public class UserMapper {
     private Database database;
 
-    public UserMapper(Database database)
-    {
+
+
+    public UserMapper(Database database) {
         this.database = database;
+
     }
 
-    public void createUser(User user) throws UserException
-    {
-        try (Connection connection = database.connect())
-        {
+
+
+    public void createUser(User user) throws UserException {
+
+        try (Connection connection = database.connect()) {
+
             String sql = "INSERT INTO user (email, password, phone, credit, role) VALUES (?, ?, ?, ?, ?)";
 
-            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
-            {
+            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
                 ps.setString(1, user.getEmail());
                 ps.setString(2, user.getPassword());
                 ps.setInt(3, user.getPhone());
@@ -35,61 +37,72 @@ public class UserMapper
 
                 int id = ids.getInt(1);
                 user.setUser_id(id);
+
             }
-            catch (SQLException ex)
-            {
+
+            catch (SQLException ex) {
                 throw new UserException(ex.getMessage());
+
             }
         }
-        catch (SQLException ex)
-        {
+
+        catch (SQLException ex) {
             throw new UserException(ex.getMessage());
+
         }
     }
 
-    public User login(String email, String password) throws UserException
-    {
-        try (Connection connection = database.connect())
-        {
+
+
+    public User login(String email, String password) throws UserException {
+
+        try (Connection connection = database.connect()) {
             String sql = "SELECT id, role FROM users WHERE email=? AND password=?";
 
-            try (PreparedStatement ps = connection.prepareStatement(sql))
-            {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ps.setString(1, email);
                 ps.setString(2, password);
                 ResultSet rs = ps.executeQuery();
-                if (rs.next())
-                {
+
+                if (rs.next()) {
                     String role = rs.getString("role");
                     int id = rs.getInt("user_id");
                     User user = new User(email, password,0,0, role);
                     user.setUser_id(id);
                     return user;
-                } else
-                {
+
+                }
+
+                else {
                     throw new UserException("Could not validate user");
+
                 }
             }
-            catch (SQLException ex)
-            {
+
+            catch (SQLException ex) {
                 throw new UserException(ex.getMessage());
+
             }
         }
-        catch (SQLException ex)
-        {
+
+        catch (SQLException ex) {
             throw new UserException("Connection to database could not be established");
+
         }
     }
 
-    public List<User> listOfStudents() throws UserException{
+
+
+    public List<User> listOfStudents() throws UserException {
         List<User> listOfStudents = new ArrayList<>();
 
-        try (Connection connection = database.connect()){
+        try (Connection connection = database.connect()) {
             String sql = "SELECT * FROM user WHERE role='student'";
 
-            try (PreparedStatement ps = connection.prepareStatement(sql)){
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ResultSet rs = ps.executeQuery();
-                while (rs.next()){
+
+                while (rs.next()) {
                     int user_id = rs.getInt("user_id");
                     String email = rs.getString("email");
                     String password = rs.getString("password");
@@ -102,12 +115,17 @@ public class UserMapper
 
                 }
             }
-        } catch (SQLException exception) {
+        }
+
+        catch (SQLException exception) {
             exception.printStackTrace();
             throw new UserException(exception.getMessage());
+
         }
+
         return listOfStudents;
     }
+
 
 
     public int useCredit(int credit, int user_id) {
@@ -120,14 +138,20 @@ public class UserMapper
                     ps.setInt(2, user_id);
                     ps.executeUpdate();
 
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
                 }
 
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            return credit;
+                catch (SQLException throwables) {
+                    throwables.printStackTrace();
 
+                }
+
+            }
+
+            catch (SQLException throwables) {
+                throwables.printStackTrace();
+
+            }
+
+            return credit;
     }
 }
