3f0f267baf7591b66b1949195610c0ebbcc835a9
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-20 18:06:07 +0200

    Update BookingFacade.java
    Updated BookingFacade java class.

--- src/main/java/business/services/BookingFacade.java
+++ src/main/java/business/services/BookingFacade.java
@@ -5,6 +5,8 @@ import business.exceptions.UserException;
 import business.persistence.BookingMapper;
 import business.persistence.Database;
 
+import java.util.List;
+
 public class BookingFacade {
 
     private BookingMapper bookingMapper;
@@ -15,11 +17,26 @@ public class BookingFacade {
 
     }
 
-    public Booking bookingAnItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
+
+    public List<Booking> listOfBookings() throws UserException {
+        return bookingMapper.listOfBookings();
+    }
+    public void updateBookings() throws  UserException {
+        bookingMapper.updateBookings();
+    }
+
+
+
+
+
+
+    public Booking bookAnItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
 
         Booking booking = new Booking (booking_date, days, comment, booking_status, user_id, item_id);
         booking = bookingMapper.bookAnItem(booking);
 
         return booking;
     }
+
+
 }
