82cb11eabe0fb02113f0a6b2289c80bdd21ecd84
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-20 18:25:18 +0200

    Update BookingMapper.java
    Created listOfBookings arraylist.

--- src/main/java/business/persistence/BookingMapper.java
+++ src/main/java/business/persistence/BookingMapper.java
@@ -4,6 +4,8 @@ import business.entities.Booking;
 import business.exceptions.UserException;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingMapper {
     Database database;
@@ -32,4 +34,51 @@ public class BookingMapper {
         return booking;
     }
 
+    public List<Booking> listOfBookings() throws UserException {
+        List<Booking> listOfBookings = new ArrayList<>();
+
+        try (Connection connection = database.connect()) {
+            String sql = "SELECT * FROM booking";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+
+                    int booking_id = rs.getInt("booking_id");
+                    String booking_date = rs.getString("booking_date");
+                    int booking_days = rs.getInt("days");
+                    String booking_comment = rs.getString("comment");
+                    boolean booking_status = rs.getBoolean("booking_status");
+                    int booking_user_id = rs.getInt("fk_user_id");
+                    int booking_item_id = rs.getInt("fk_item_id");
+
+
+                    Booking booking = new Booking(booking_date, booking_days, booking_comment, booking_status,booking_user_id, booking_item_id);
+                    booking.setBooking_id(booking_id);
+                    listOfBookings.add(booking);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return listOfBookings;
+    }
+
+    public void updateBookings() throws UserException {
+
+        try (Connection connection = database.connect()) {
+            String sql = "update booking SET booking_status = ?";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+                ps.setBoolean(1, true);
+                ps.executeUpdate();
+
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
