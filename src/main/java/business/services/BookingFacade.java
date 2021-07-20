c4268b35bef99f12a76461a6a50aeef69040dba7
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-20 19:03:21 +0200

    Update BookingFacade.java
    Updated code formatting.

--- src/main/java/business/services/BookingFacade.java
+++ src/main/java/business/services/BookingFacade.java
@@ -12,23 +12,16 @@ public class BookingFacade {
     private BookingMapper bookingMapper;
 
     public BookingFacade(Database database) {
-
         this.bookingMapper = new BookingMapper(database);
-
     }
-
 
     public List<Booking> listOfBookings() throws UserException {
         return bookingMapper.listOfBookings();
     }
+
     public void updateBookings() throws  UserException {
         bookingMapper.updateBookings();
     }
-
-
-
-
-
 
     public Booking bookAnItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
 
