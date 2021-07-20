10289fd4f387a4829c02bef4bbae236a1e411ec4
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-20 16:09:55 +0200

    Create ItemFacade.java
    Created ItemFacade java class.

---
+++ src/main/java/business/services/ItemFacade.java
@@ -0,0 +1,20 @@
+package business.services;
+
+import business.entities.Item;
+import business.exceptions.UserException;
+import business.persistence.Database;
+import business.persistence.ItemMapper;
+
+import java.util.List;
+
+public class ItemFacade {
+    ItemMapper itemMapper;
+
+    public ItemFacade(Database database) {
+        this.itemMapper = new ItemMapper(database);
+    }
+
+    public List<Item> listOfItems() throws UserException{
+        return itemMapper.listOfItems();
+    }
+}
