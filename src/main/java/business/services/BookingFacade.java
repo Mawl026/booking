d332e290ed3f81b00290a1f57be4a17524062b65
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2021-07-21 18:52:17 +0200

    Created CreateBookingCommand and polished stuff.
    Title.

--- src/main/java/business/services/BookingFacade.java
+++ src/main/java/business/services/BookingFacade.java
@@ -19,11 +19,11 @@ public class BookingFacade {
         return bookingMapper.listOfBookings();
     }
 
-    public void updateBookings() throws  UserException {
+    public void updateBooking() throws  UserException {
         bookingMapper.updateBookings();
     }
 
-    public Booking bookAnItem(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
+    public Booking createBooking(String booking_date, int days, String comment, boolean booking_status, int user_id, int item_id) throws UserException {
 
         Booking booking = new Booking (booking_date, days, comment, booking_status, user_id, item_id);
         booking = bookingMapper.createBooking(booking);
